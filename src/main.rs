bdea43047e0674324e73180563b2f83514943f87
SamNgigi <sam.t.ngigi@gmail.com>
2026-03-13 18:54:07 +0300

    [feat]: Adds crud endpoints and tests them using api.http. Working

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,15 @@ use uuid::Uuid;
 
 use config::{AppConfig, create_pool};
 
-#[allow(dead_code)]
 #[derive(Deserialize)]
 struct UserPayload {
     name: String,
     email: String,
 }
 
-#[allow(dead_code)]
-#[derive(Serialize)]
+#[derive(Serialize, FromRow)]
 struct User {
+    id: i64,
     uuid: Uuid,
     name: String,
     email: String,
@@ -44,14 +43,100 @@ async fn main() {
         .await
         .expect("Migration Failed!");
 
-    let app = Router::new().route("/", get(root)).with_state(pool);
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user).get(list_users))
+        .route(
+            "/users/{id}",
+            get(get_user_by_id).put(update_user).delete(delete_user),
+        )
+        .with_state(pool);
 
     let addr = format!("{}:{}", &config.app_host, &config.app_port);
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .expect("Failed to bind to {addr}");
     println!("🚀 Server running on port 8000");
     axum::serve(listener, app).await.unwrap();
 }
 
+// -----------------------------------------------------------------
+// ENDPOINT HANDLERS
+// -----------------------------------------------------------------
+
+// Test Endpoint
 async fn root() -> &'static str {
     "Welcome to Basic Crud User Management"
+}
+
+// CREATE USER
+async fn create_user(
+    State(pool): State<PgPool>,
+    Json(payload): Json<UserPayload>,
+) -> Result<(StatusCode, Json<User>), StatusCode> {
+    sqlx::query_as::<_, User>("INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *")
+        .bind(payload.name)
+        .bind(payload.email)
+        .fetch_one(&pool)
+        .await
+        .map(|u| (StatusCode::CREATED, Json(u)))
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+// GET ALL USERS
+async fn list_users(State(pool): State<PgPool>) -> Result<Json<Vec<User>>, StatusCode> {
+    sqlx::query_as::<_, User>("SELECT * FROM users")
+        .fetch_all(&pool)
+        .await
+        .map(Json)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+// GET USER BY ID
+async fn get_user_by_id(
+    State(pool): State<PgPool>,
+    Path(id): Path<i64>,
+) -> Result<Json<User>, StatusCode> {
+    sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = $1")
+        .bind(id)
+        .fetch_one(&pool)
+        .await
+        .map(Json)
+        .map_err(|_| StatusCode::NOT_FOUND)
+}
+
+// UPDATE USER
+async fn update_user(
+    State(pool): State<PgPool>,
+    Path(id): Path<i64>,
+    Json(payload): Json<UserPayload>,
+) -> Result<Json<User>, StatusCode> {
+    sqlx::query_as::<_, User>(
+        "UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING id, name, email",
+    )
+    .bind(payload.name)
+    .bind(payload.email)
+    .bind(id)
+    .fetch_one(&pool)
+    .await
+    .map(Json)
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+// DELETE USER
+async fn delete_user(
+    State(pool): State<PgPool>,
+    Path(id): Path<i64>,
+) -> Result<StatusCode, StatusCode> {
+    let result = sqlx::query("DELETE FROM users WHERE id = $1")
+        .bind(id)
+        .execute(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    if result.rows_affected() == 0 {
+        Err(StatusCode::NOT_FOUND)
+    } else {
+        Ok(StatusCode::NO_CONTENT)
+    }
 }
